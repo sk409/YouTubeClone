93194e7134047cf31b5eca5be25396c1128dce6c
Shoutokobayashi <deepstrap353@gmail.com>
2019-06-22 15:20:20 +0900

    Initial Commit

---
+++ YouTubeClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by 小林聖人 on 2019/06/22.
+//  Copyright © 2019 小林聖人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
